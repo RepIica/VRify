d42b7bb0ae16f6403d477885f06589220eec958c
Sammy Akharaz <ketrixz@gmail.com>
2018-08-20 11:09:52 -0400

    centered login with semantic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, Header } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { submitLogin } from '../actions/authActions.js'
 
@@ -16,17 +17,25 @@ class Login extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-            <h1>Login</h1>
+      <div id="ui-container">
+        <Grid verticalAlign='middle' centered>
+          <Grid.Row>
+            <Grid.Column id="login-align"></Grid.Column>
+            <Grid.Column mobile={14} tablet={3} computer={3}>
+              <Header as="h1" textAlign="center" inverted color="grey" id='login-h1'>Login</Header>
               <form onSubmit={(e) => {
-                e.preventDefault()
-                this.props.submitLogin(this.state.email, this.state.password)
-              }}>
-              <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.onInputChange}/><br/><br/>
-              <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.onInputChange}/><br/><br/>
-              <input type="submit" value="Log In"/>
-            </form>
-      </React.Fragment>
+                  e.preventDefault()
+                  this.props.submitLogin(this.state.email, this.state.password)
+                }}>
+                <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.onInputChange}/><br/><br/>
+                <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.onInputChange}/><br/><br/>
+                <input type="submit" value="Log In"/>
+              </form>
+            </Grid.Column>
+            <Grid.Column></Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     )
   }
 }
