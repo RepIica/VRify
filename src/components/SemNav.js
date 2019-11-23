93d08cd7d69d09c0de98abf9cfba1600ac5fcea6
RepIica <ketrixz@gmail.com>
2019-11-22 19:35:20 -0500

    prettier

--- src/components/SemNav.js
+++ src/components/SemNav.js
@@ -1,57 +1,57 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Dropdown, Menu, Button } from 'semantic-ui-react'
+import React from "react";
+import { connect } from "react-redux";
+import { Dropdown, Menu, Button } from "semantic-ui-react";
 
 class SemNav extends React.Component {
-
-  projName = (id) => {
+  projName = id => {
     return fetch(`http://localhost:3001/api/v1/user/projects`, {
       headers: {
-        'Content-Type': 'application/json',
-        Accepts: 'application/json',
+        "Content-Type": "application/json",
+        Accepts: "application/json",
       },
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        userId: id
-      })
-    }).then(resp => resp.json())
-  }
+        userId: id,
+      }),
+    }).then(resp => resp.json());
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('<SemNav> componentDidUpdated')
-    console.log(this.props)
+    console.log("<SemNav> componentDidUpdated");
+    console.log(this.props);
     if (this.props.currentUser && this.props.currentProj) {
-          const dlBtn = document.querySelector('.dl')
-          if (dlBtn) {
-            dlBtn.href=`./${this.props.currentUser.name}/${this.props.currentProj.name}.html`
-          }else{
-            console.error('no download button on page')
-          }
+      const dlBtn = document.querySelector(".dl");
+      if (dlBtn) {
+        dlBtn.href = `./${this.props.currentUser.name}/${this.props.currentProj.name}.html`;
+      } else {
+        console.error("no download button on page");
+      }
     }
-
   }
 
   mobileToggle = () => {
-    const el = document.querySelector('.right.menu')
-    if (el.style.position==='static'){
-      el.style.position = 'absolute'
-      el.style.visibility = 'hidden'
-    }else{
-      el.style.position = 'static'
-      el.style.visibility = 'visible'
+    const el = document.querySelector(".right.menu");
+    if (el.style.position === "static") {
+      el.style.position = "absolute";
+      el.style.visibility = "hidden";
+    } else {
+      el.style.position = "static";
+      el.style.visibility = "visible";
     }
-  }
+  };
 
   render() {
-    console.warn(this.props.projects)
+    console.warn(this.props.projects);
     return (
       <Menu stackable id="nav">
         <Menu.Item
-          name='home'
-          onClick={(e) => {this.props.history.push('/')}}
-          content='VRify'
+          name="home"
+          onClick={e => {
+            this.props.history.push("/");
+          }}
+          content="VRify"
           // children={<span>IFY</span>}
-          id ='logo'
+          id="logo"
         />
 
         <Button onClick={this.mobileToggle} id="mobile-btn">
@@ -60,65 +60,69 @@ class SemNav extends React.Component {
           <span className="icon-bar"></span>
         </Button>
 
-        <Menu.Menu position='right'>
-          {this.props.currentUser ?
+        <Menu.Menu position="right">
+          {this.props.currentUser ? (
             <React.Fragment>
-              <Dropdown item text='MY PROJECTS' className="navlink1">
+              <Dropdown item text="MY PROJECTS" className="navlink1">
                 <Dropdown.Menu>
-
-                  {this.props.projects ?
-                    this.props.projects.map((project) => <Dropdown.Item key={project.id}>{project.name}</Dropdown.Item>)
-                    :
+                  {this.props.projects ? (
+                    this.props.projects.map(project => (
+                      <Dropdown.Item key={project.id}>
+                        {project.name}
+                      </Dropdown.Item>
+                    ))
+                  ) : (
                     <Dropdown.Item>No Projects Yet!</Dropdown.Item>
-                  }
-
+                  )}
                 </Dropdown.Menu>
               </Dropdown>
               <Menu.Item
-                name='SAVE PROJECT'
+                name="SAVE PROJECT"
                 className="navlink1"
                 onClick={this.props.save}
                 // onClick={(e) => {this.props.save();this.props.setProj()}}
                 download
               />
-              {this.props.currentProj &&
+              {this.props.currentProj && (
                 <Menu.Item
-                  name='DOWNLOAD'
+                  name="DOWNLOAD"
                   className="dl navlink1"
-                  onClick={(e) => {
-                    console.log(`downloading ${e.target.href}`)
+                  onClick={e => {
+                    console.log(`downloading ${e.target.href}`);
                   }}
-                  href={this.props.projects[this.props.projects.length-1].filepath}
+                  href={
+                    this.props.projects[this.props.projects.length - 1].filepath
+                  }
                   download
                 />
-              }
+              )}
               <Menu.Item
-                name='LOGOUT'
+                name="LOGOUT"
                 onClick={this.props.logout}
                 className="navlink1"
               />
             </React.Fragment>
-            :
+          ) : (
             <Menu.Item
-              name='SIGNUP'
-              onClick={(e) => {this.props.history.push('/signup')}}
+              name="SIGNUP"
+              onClick={e => {
+                this.props.history.push("/signup");
+              }}
               className="navlink1"
             />
-          }
+          )}
         </Menu.Menu>
       </Menu>
-    )
+    );
   }
-
 }
 
-
-const mapStateToProps = (state) => {
-  return{
+const mapStateToProps = state => {
+  return {
     currentUser: state.authReducer.currentUser,
     projects: state.projectsReducer.projects,
-    currentProj: state.projectsReducer.currentProject
-  }
-}
+    currentProj: state.projectsReducer.currentProject,
+  };
+};
 
-export default connect(mapStateToProps)(SemNav)
+export default connect(mapStateToProps)(SemNav);
